390cda463250db2536485f6f124f63df5dd96a37
Branch Vincent <branchevincent@gmail.com>
2021-04-05 20:24:43 -0400

    split out codegen

--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -66,10 +66,10 @@ export type CreateUserPayload = {
    * unchanged and unused. May be used by a client to track mutations.
    */
   clientMutationId?: Maybe<Scalars['String']>;
-  /** The `User` that was created by this mutation. */
-  user?: Maybe<User>;
   /** Our root query field type. Allows us to run any query from our mutation payload. */
   query?: Maybe<Query>;
+  /** The `User` that was created by this mutation. */
+  user?: Maybe<User>;
   /** An edge for our `User`. May be used by Relay 1. */
   userEdge?: Maybe<UsersEdge>;
 };
@@ -112,11 +112,11 @@ export type DeleteUserPayload = {
    * unchanged and unused. May be used by a client to track mutations.
    */
   clientMutationId?: Maybe<Scalars['String']>;
-  /** The `User` that was deleted by this mutation. */
-  user?: Maybe<User>;
   deletedUserNodeId?: Maybe<Scalars['ID']>;
   /** Our root query field type. Allows us to run any query from our mutation payload. */
   query?: Maybe<Query>;
+  /** The `User` that was deleted by this mutation. */
+  user?: Maybe<User>;
   /** An edge for our `User`. May be used by Relay 1. */
   userEdge?: Maybe<UsersEdge>;
 };
@@ -131,50 +131,20 @@ export type DeleteUserPayloadUserEdgeArgs = {
 /** The root mutation type which contains root level fields which mutate data. */
 export type Mutation = {
   __typename?: 'Mutation';
-  /** Creates a single `User`. */
-  createUser?: Maybe<CreateUserPayload>;
-  /** Updates a single `User` using its globally unique id and a patch. */
-  updateUserByNodeId?: Maybe<UpdateUserPayload>;
-  /** Updates a single `User` using a unique key and a patch. */
-  updateUser?: Maybe<UpdateUserPayload>;
-  /** Deletes a single `User` using its globally unique id. */
-  deleteUserByNodeId?: Maybe<DeleteUserPayload>;
-  /** Deletes a single `User` using a unique key. */
-  deleteUser?: Maybe<DeleteUserPayload>;
   /** Creates a JWT token that will securely identify a person and give them certain permissions. This token expires in 2 days. */
   authenticate?: Maybe<AuthenticatePayload>;
+  /** Creates a single `User`. */
+  createUser?: Maybe<CreateUserPayload>;
+  /** Deletes a single `User` using a unique key. */
+  deleteUser?: Maybe<DeleteUserPayload>;
+  /** Deletes a single `User` using its globally unique id. */
+  deleteUserByNodeId?: Maybe<DeleteUserPayload>;
   /** Registers a new user and creates an account. */
   registerUser?: Maybe<RegisterUserPayload>;
-};
-
-
-/** The root mutation type which contains root level fields which mutate data. */
-export type MutationCreateUserArgs = {
-  input: CreateUserInput;
-};
-
-
-/** The root mutation type which contains root level fields which mutate data. */
-export type MutationUpdateUserByNodeIdArgs = {
-  input: UpdateUserByNodeIdInput;
-};
-
-
-/** The root mutation type which contains root level fields which mutate data. */
-export type MutationUpdateUserArgs = {
-  input: UpdateUserInput;
-};
-
-
-/** The root mutation type which contains root level fields which mutate data. */
-export type MutationDeleteUserByNodeIdArgs = {
-  input: DeleteUserByNodeIdInput;
-};
-
-
-/** The root mutation type which contains root level fields which mutate data. */
-export type MutationDeleteUserArgs = {
-  input: DeleteUserInput;
+  /** Updates a single `User` using a unique key and a patch. */
+  updateUser?: Maybe<UpdateUserPayload>;
+  /** Updates a single `User` using its globally unique id and a patch. */
+  updateUserByNodeId?: Maybe<UpdateUserPayload>;
 };
 
 
@@ -185,8 +155,38 @@ export type MutationAuthenticateArgs = {
 
 
 /** The root mutation type which contains root level fields which mutate data. */
+export type MutationCreateUserArgs = {
+  input: CreateUserInput;
+};
+
+
+/** The root mutation type which contains root level fields which mutate data. */
+export type MutationDeleteUserArgs = {
+  input: DeleteUserInput;
+};
+
+
+/** The root mutation type which contains root level fields which mutate data. */
+export type MutationDeleteUserByNodeIdArgs = {
+  input: DeleteUserByNodeIdInput;
+};
+
+
+/** The root mutation type which contains root level fields which mutate data. */
 export type MutationRegisterUserArgs = {
   input: RegisterUserInput;
+};
+
+
+/** The root mutation type which contains root level fields which mutate data. */
+export type MutationUpdateUserArgs = {
+  input: UpdateUserInput;
+};
+
+
+/** The root mutation type which contains root level fields which mutate data. */
+export type MutationUpdateUserByNodeIdArgs = {
+  input: UpdateUserByNodeIdInput;
 };
 
 /** An object with a globally unique `ID`. */
@@ -198,55 +198,43 @@ export type Node = {
 /** Information about pagination in a connection. */
 export type PageInfo = {
   __typename?: 'PageInfo';
+  /** When paginating forwards, the cursor to continue. */
+  endCursor?: Maybe<Scalars['Cursor']>;
   /** When paginating forwards, are there more items? */
   hasNextPage: Scalars['Boolean'];
   /** When paginating backwards, are there more items? */
   hasPreviousPage: Scalars['Boolean'];
   /** When paginating backwards, the cursor to continue. */
   startCursor?: Maybe<Scalars['Cursor']>;
-  /** When paginating forwards, the cursor to continue. */
-  endCursor?: Maybe<Scalars['Cursor']>;
 };
 
 /** The root query type which gives access points into the data universe. */
 export type Query = Node & {
   __typename?: 'Query';
+  /** Gets the user who was identified by our JWT. */
+  currentUser?: Maybe<User>;
+  /** Gets the user id who was identified by our JWT. */
+  currentUserId?: Maybe<Scalars['Int']>;
+  /** Fetches an object given its globally unique `ID`. */
+  node?: Maybe<Node>;
+  /** The root query type must be a `Node` to work well with Relay 1 mutations. This just resolves to `query`. */
+  nodeId: Scalars['ID'];
   /**
    * Exposes the root query type nested one level down. This is helpful for Relay 1
    * which can only query top level fields if they are in a particular form.
    */
   query: Query;
-  /** The root query type must be a `Node` to work well with Relay 1 mutations. This just resolves to `query`. */
-  nodeId: Scalars['ID'];
-  /** Fetches an object given its globally unique `ID`. */
-  node?: Maybe<Node>;
-  /** Reads and enables pagination through a set of `User`. */
-  users?: Maybe<UsersConnection>;
   user?: Maybe<User>;
-  /** Gets the user who was identified by our JWT. */
-  currentUser?: Maybe<User>;
-  /** Gets the user id who was identified by our JWT. */
-  currentUserId?: Maybe<Scalars['Int']>;
   /** Reads a single `User` using its globally unique `ID`. */
   userByNodeId?: Maybe<User>;
+  /** Reads and enables pagination through a set of `User`. */
+  users?: Maybe<UsersConnection>;
 };
 
 
 /** The root query type which gives access points into the data universe. */
 export type QueryNodeArgs = {
   nodeId: Scalars['ID'];
-};
-
-
-/** The root query type which gives access points into the data universe. */
-export type QueryUsersArgs = {
-  first?: Maybe<Scalars['Int']>;
-  last?: Maybe<Scalars['Int']>;
-  offset?: Maybe<Scalars['Int']>;
-  before?: Maybe<Scalars['Cursor']>;
-  after?: Maybe<Scalars['Cursor']>;
-  orderBy?: Maybe<Array<UsersOrderBy>>;
-  condition?: Maybe<UserCondition>;
 };
 
 
@@ -261,6 +249,18 @@ export type QueryUserByNodeIdArgs = {
   nodeId: Scalars['ID'];
 };
 
+
+/** The root query type which gives access points into the data universe. */
+export type QueryUsersArgs = {
+  after?: Maybe<Scalars['Cursor']>;
+  before?: Maybe<Scalars['Cursor']>;
+  condition?: Maybe<UserCondition>;
+  first?: Maybe<Scalars['Int']>;
+  last?: Maybe<Scalars['Int']>;
+  offset?: Maybe<Scalars['Int']>;
+  orderBy?: Maybe<Array<UsersOrderBy>>;
+};
+
 /** All input for the `registerUser` mutation. */
 export type RegisterUserInput = {
   /**
@@ -268,9 +268,9 @@ export type RegisterUserInput = {
    * payload verbatim. May be used to track mutations by the client.
    */
   clientMutationId?: Maybe<Scalars['String']>;
+  email: Scalars['String'];
   firstName: Scalars['String'];
   lastName: Scalars['String'];
-  email: Scalars['String'];
   password: Scalars['String'];
 };
 
@@ -282,9 +282,9 @@ export type RegisterUserPayload = {
    * unchanged and unused. May be used by a client to track mutations.
    */
   clientMutationId?: Maybe<Scalars['String']>;
-  user?: Maybe<User>;
   /** Our root query field type. Allows us to run any query from our mutation payload. */
   query?: Maybe<Query>;
+  user?: Maybe<User>;
   /** An edge for our `User`. May be used by Relay 1. */
   userEdge?: Maybe<UsersEdge>;
 };
@@ -315,10 +315,10 @@ export type UpdateUserInput = {
    * payload verbatim. May be used to track mutations by the client.
    */
   clientMutationId?: Maybe<Scalars['String']>;
-  /** An object where the defined keys will be set on the `User` being updated. */
-  patch: UserPatch;
   /** The primary unique identifier for the user. */
   id: Scalars['Int'];
+  /** An object where the defined keys will be set on the `User` being updated. */
+  patch: UserPatch;
 };
 
 /** The output of our update `User` mutation. */
@@ -329,10 +329,10 @@ export type UpdateUserPayload = {
    * unchanged and unused. May be used by a client to track mutations.
    */
   clientMutationId?: Maybe<Scalars['String']>;
-  /** The `User` that was updated by this mutation. */
-  user?: Maybe<User>;
   /** Our root query field type. Allows us to run any query from our mutation payload. */
   query?: Maybe<Query>;
+  /** The `User` that was updated by this mutation. */
+  user?: Maybe<User>;
   /** An edge for our `User`. May be used by Relay 1. */
   userEdge?: Maybe<UsersEdge>;
 };
@@ -346,60 +346,60 @@ export type UpdateUserPayloadUserEdgeArgs = {
 /** A user of the app. */
 export type User = Node & {
   __typename?: 'User';
-  /** A globally unique identifier. Can be used in various places throughout the system to identify this single value. */
-  nodeId: Scalars['ID'];
-  /** The primary unique identifier for the user. */
-  id: Scalars['Int'];
-  /** The user’s first name. */
-  firstName: Scalars['String'];
-  /** The user’s last name. */
-  lastName?: Maybe<Scalars['String']>;
   /** The time this user was created. */
   createdAt?: Maybe<Scalars['Datetime']>;
-  /** The time this user was updated. */
-  updatedAt?: Maybe<Scalars['Datetime']>;
+  /** The user’s first name. */
+  firstName: Scalars['String'];
   /** The user’s full name. */
   fullName?: Maybe<Scalars['String']>;
+  /** The primary unique identifier for the user. */
+  id: Scalars['Int'];
+  /** The user’s last name. */
+  lastName?: Maybe<Scalars['String']>;
+  /** A globally unique identifier. Can be used in various places throughout the system to identify this single value. */
+  nodeId: Scalars['ID'];
+  /** The time this user was updated. */
+  updatedAt?: Maybe<Scalars['Datetime']>;
 };
 
 /** A condition to be used against `User` object types. All fields are tested for equality and combined with a logical ‘and.’ */
 export type UserCondition = {
-  /** Checks for equality with the object’s `id` field. */
-  id?: Maybe<Scalars['Int']>;
-  /** Checks for equality with the object’s `firstName` field. */
-  firstName?: Maybe<Scalars['String']>;
-  /** Checks for equality with the object’s `lastName` field. */
-  lastName?: Maybe<Scalars['String']>;
   /** Checks for equality with the object’s `createdAt` field. */
   createdAt?: Maybe<Scalars['Datetime']>;
+  /** Checks for equality with the object’s `firstName` field. */
+  firstName?: Maybe<Scalars['String']>;
+  /** Checks for equality with the object’s `id` field. */
+  id?: Maybe<Scalars['Int']>;
+  /** Checks for equality with the object’s `lastName` field. */
+  lastName?: Maybe<Scalars['String']>;
   /** Checks for equality with the object’s `updatedAt` field. */
   updatedAt?: Maybe<Scalars['Datetime']>;
 };
 
 /** An input for mutations affecting `User` */
 export type UserInput = {
-  /** The primary unique identifier for the user. */
-  id?: Maybe<Scalars['Int']>;
-  /** The user’s first name. */
-  firstName: Scalars['String'];
-  /** The user’s last name. */
-  lastName?: Maybe<Scalars['String']>;
   /** The time this user was created. */
   createdAt?: Maybe<Scalars['Datetime']>;
+  /** The user’s first name. */
+  firstName: Scalars['String'];
+  /** The primary unique identifier for the user. */
+  id?: Maybe<Scalars['Int']>;
+  /** The user’s last name. */
+  lastName?: Maybe<Scalars['String']>;
   /** The time this user was updated. */
   updatedAt?: Maybe<Scalars['Datetime']>;
 };
 
 /** Represents an update to a `User`. Fields that are set will be updated. */
 export type UserPatch = {
-  /** The primary unique identifier for the user. */
-  id?: Maybe<Scalars['Int']>;
-  /** The user’s first name. */
-  firstName?: Maybe<Scalars['String']>;
-  /** The user’s last name. */
-  lastName?: Maybe<Scalars['String']>;
   /** The time this user was created. */
   createdAt?: Maybe<Scalars['Datetime']>;
+  /** The user’s first name. */
+  firstName?: Maybe<Scalars['String']>;
+  /** The primary unique identifier for the user. */
+  id?: Maybe<Scalars['Int']>;
+  /** The user’s last name. */
+  lastName?: Maybe<Scalars['String']>;
   /** The time this user was updated. */
   updatedAt?: Maybe<Scalars['Datetime']>;
 };
@@ -407,10 +407,10 @@ export type UserPatch = {
 /** A connection to a list of `User` values. */
 export type UsersConnection = {
   __typename?: 'UsersConnection';
-  /** A list of `User` objects. */
-  nodes: Array<User>;
   /** A list of edges which contains the `User` and cursor to aid in pagination. */
   edges: Array<UsersEdge>;
+  /** A list of `User` objects. */
+  nodes: Array<User>;
   /** Information to aid in pagination. */
   pageInfo: PageInfo;
   /** The count of *all* `User` you could get from the connection. */
@@ -428,17 +428,17 @@ export type UsersEdge = {
 
 /** Methods to use when ordering `User`. */
 export enum UsersOrderBy {
-  NATURAL = 'NATURAL',
-  ID_ASC = 'ID_ASC',
-  ID_DESC = 'ID_DESC',
-  FIRST_NAME_ASC = 'FIRST_NAME_ASC',
-  FIRST_NAME_DESC = 'FIRST_NAME_DESC',
-  LAST_NAME_ASC = 'LAST_NAME_ASC',
-  LAST_NAME_DESC = 'LAST_NAME_DESC',
   CREATED_AT_ASC = 'CREATED_AT_ASC',
   CREATED_AT_DESC = 'CREATED_AT_DESC',
-  UPDATED_AT_ASC = 'UPDATED_AT_ASC',
-  UPDATED_AT_DESC = 'UPDATED_AT_DESC',
+  FIRST_NAME_ASC = 'FIRST_NAME_ASC',
+  FIRST_NAME_DESC = 'FIRST_NAME_DESC',
+  ID_ASC = 'ID_ASC',
+  ID_DESC = 'ID_DESC',
+  LAST_NAME_ASC = 'LAST_NAME_ASC',
+  LAST_NAME_DESC = 'LAST_NAME_DESC',
+  NATURAL = 'NATURAL',
   PRIMARY_KEY_ASC = 'PRIMARY_KEY_ASC',
-  PRIMARY_KEY_DESC = 'PRIMARY_KEY_DESC'
+  PRIMARY_KEY_DESC = 'PRIMARY_KEY_DESC',
+  UPDATED_AT_ASC = 'UPDATED_AT_ASC',
+  UPDATED_AT_DESC = 'UPDATED_AT_DESC'
 }
